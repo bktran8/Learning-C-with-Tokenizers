63d3efef15c10dc2081435976dbcb8c093b7c582
Brenda <bktran8@gmail.com>
2017-06-02 13:03:55 -0700

    add prog1_2

---
+++ prog1_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char str[66];
+    char *ptr;
+    
+    printf("Assignment #1-1, Brenda Tran, bktran8@gmail.com\n");
+    
+    printf(">");
+    fgets(str, 66, stdin);
+   
+    ptr = strtok(str, " \n");
+    while (ptr != '\0'){
+        if((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'B')){
+            printf("STR ");
+        } else if(*ptr >= '0' && *ptr <= '9'){
+            printf("INT ");
+        }
+        ptr = strtok('\0', " \n");
+    }   
+   printf("\n");
+   return 0;
+}
+
+//prints letter by letter, number 
