59e1328d11045807160e8c034ba910e7f015c7f2
Brenda <bktran8@gmail.com>
2017-06-06 13:42:18 -0700

    add prog1_4

--- prog1_4.c
+++ prog1_4.c
@@ -6,63 +6,59 @@ int main(){
     char str[256];
     char *ptr;
     char str1[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char str2[256];
     int  count=1;
-    int  charcount = 0;
-    int  b=0;
-    int  d=0;
+    int  charcount;
+    int i;
     
     printf("Assignment #1-4, Brenda Tran, bktran8@gmail.com\n");
     
     for(;;){
     printf(">");
-    fgets(str, 21, stdin);
-        while(str[b]){
-            count = 1;
-            while(str[b] != '\0') {
-                if(str[b] == ' ' || str[b] == '\n'){
-                    count++;
-                }
-            b++;
-            }
-            count--;
-            if(count>2){
-                printf("ERROR! Incorrect number of tokens found.\n");
-                printf(">");
-                fgets(str, 256, stdin);
-                continue;
-             }
-             charcount = 0;
-                while(str[d] != '\0'){
-                    charcount++;
-                    d++;
-                }   
-             charcount--;
-                if (charcount>20){
-                    printf("ERROR! Input string too long.\n");
-                    printf(">");
-                    fgets(str, 256, stdin);
-                    continue;
-                }
-        }
+    fgets(str, 256, stdin);
+    strcpy(str2,str);
     ptr = strtok(str, " \n");
-    while (ptr != '\0'){
-        if(strcasecmp(str, "quit") ==0){
+    count =0;
+    while(ptr != '\0'){
+        count++;
+        ptr = strtok('\0', " \n");
+    }
+    charcount = 0;
+    for(i = 0; str[i]; i++){
+        if(str[i] != ' '){
+        charcount++;
+        }
+    }
+            if(str2[0] == '\n' || str2[0] == ' '||count>2 ){
+                printf("ERROR! Incorrect number of tokens found.\n");
+                count=0;
+                charcount = 0;
+            }else if (charcount>21){
+                    printf("ERROR! Input string too long.\n");
+                    count = 0;
+                    charcount=0;
+            
+            }else{
+        ptr = strtok(str2, " \n");
+        while (ptr != '\0'){
+        if(strcasecmp(str, "quit") ==0 && count==1){
             exit(1);
         }
-    if((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')){
-        printf("STR ");
-    }else if(*ptr >= '0' && *ptr <= '9'){
-        if(strpbrk(ptr,str1)){ 
+            if((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')){
             printf("STR ");
-        }else if(strchr(ptr,'.')){
-            printf("STR "); 
-        }else{
+            }else if(*ptr >= '0' && *ptr <= '9'){
+            if(strpbrk(ptr,str1)){ 
+                printf("STR ");
+            }else if(strchr(ptr,'.')){
+               printf("STR "); 
+            }else{
             printf("INT ");
+            }
         }
+        ptr = strtok('\0', " \n");
     }
-    ptr = strtok('\0', " \n");
-    }      
     printf("\n");  
-    }
-return 0; 
- }
+}
+}
+return 0;
+   }
