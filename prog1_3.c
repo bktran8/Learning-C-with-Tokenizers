8f89c364fae08fac20f02859016f8f63fdc9cac4
Brenda <bktran8@gmail.com>
2017-06-06 11:39:39 -0700

    add prog1_3

--- prog1_3.c
+++ prog1_3.c
@@ -5,6 +5,7 @@ int main(){
     char str[256];
     char *ptr;
     char str1[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char str2[256];
     int  count=1;
     int  b=0;
     
@@ -13,32 +14,19 @@ int main(){
     while(str[b]!= '\0'){
     printf(">");
     fgets(str, 66, stdin);
-        if (str[0] == '\n'){
-            printf("ERROR! Incorrect number of tokens found.\n");
-                continue;
-        }
-        if(str[0] == ' '){
-            printf("ERROR! Incorrect number of tokens found.\n");
-                continue;
-        } 
-        count = 1;
-        while(str[b] != '\0') {
-            if(str[b] == ' ' || str[b] == '\n'){
-                count++;
-            }
-            b++;
-        }
-        count--;
-            if(count>2){
-                printf("ERROR! Incorrect number of tokens found.\n");
-                printf(">");
-                fgets(str, 66, stdin);
-                continue;
-        }
-    }
+    strcpy(str2,str);
     ptr = strtok(str, " \n");
-    while (ptr != '\0'){
-        if((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')){
+    while(ptr != '\0'){
+        count++;
+        ptr = strtok('\0', " \n");
+    }
+            if(str2[0] == '\n' || str2[0] == ' ' ||count>2 ){
+                printf("ERROR! Incorrect number of tokens found.\n");
+                count=0;
+        }else{
+        ptr = strtok(str2, " \n");
+        while (ptr != '\0'){
+            if((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')){
             printf("STR ");
         }else if(*ptr >= '0' && *ptr <= '9'){
             if(strpbrk(ptr,str1)){ 
@@ -50,7 +38,9 @@ int main(){
             }
         }
         ptr = strtok('\0', " \n");
-    }   
-   printf("\n");
-   return 0;
+    }
+    printf("\n");
+    return 0;  
 }
+}
+   }
